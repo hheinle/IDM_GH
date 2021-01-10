c518cc44e362c59671f1010f946330a33483ca46
hheinle <hheinle789@gmail.com>
2021-01-10 16:25:08 +0100

    WIP : Python compiler and interpretor in xtend

--- org.xtext.idmGH.mlregDsl.tests/src/org/xtext/idmGH/mlregDsl/tests/MLRegParsingTest.xtend
+++ org.xtext.idmGH.mlregDsl.tests/src/org/xtext/idmGH/mlregDsl/tests/MLRegParsingTest.xtend
@@ -17,98 +17,98 @@ import org.xtext.idmGH.mlregDsl.mLReg.Model
 class MLRegParsingTest {
 	@Inject
 	ParseHelper<Model> parseHelper
-	
+
 	@Test
 	def void testLinearReg1() {
 		val result = parseHelper.parse('''
-			regression{
-				file: "datasets/salary_data.csv",
-				testSize : 3,
-				predictiveVariables : {0},
-				 targetVariable: 1,
-				 algorithm: Linear,
-				 errorType: mean_squared_error
-			}		''')
+		regression{
+			file: "datasets/salary_data.csv",
+			testSize : 3,
+			predictiveVariables : {0},
+			 targetVariable: 1,
+			 algorithm: Linear,
+			 errorType: mean_squared_error
+		}		''')
 		Assertions.assertNotNull(result)
 		val errors = result.eResource.errors
 		Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
-		
+
 		val PythonCompiler cmpPython = new PythonCompiler(result)
 		cmpPython.compileAndRun
 	}
-	
+
 	@Test
 	def void testLinearReg2() {
 		val result = parseHelper.parse('''
-			regression{
-				file: "datasets/ozone.csv",
-				testSize : 3,
-				predictiveVariables : {1,2,3,4,5,6,7,8,9,10},
-				 targetVariable: 0,
-				 algorithm: Linear,
-				 errorType: r2_score
-			}		''')
+		regression{
+			file: "datasets/ozone.csv",
+			testSize : 3,
+			predictiveVariables : {1,2,3,4,5,6,7,8,9,10},
+			 targetVariable: 0,
+			 algorithm: Linear,
+			 errorType: r2_score
+		}		''')
 		Assertions.assertNotNull(result)
 		val errors = result.eResource.errors
 		Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
-		
+
 		val PythonCompiler cmpPython = new PythonCompiler(result)
 		cmpPython.compileAndRun
 	}
-	
+
 	@Test
 	def void testLinearReg3() {
 		val result = parseHelper.parse('''
-			regression{
-				file: "datasets/ozone.csv",
-				testSize : 3,
-				predictiveVariables : {1,2,9,10},
-				 targetVariable: 0,
-				 algorithm: Linear,
-				 errorType: r2_score
-			}		''')
+		regression{
+			file: "datasets/ozone.csv",
+			testSize : 3,
+			predictiveVariables : {1,2,9,10},
+			 targetVariable: 0,
+			 algorithm: Linear,
+			 errorType: r2_score
+		}		''')
 		Assertions.assertNotNull(result)
 		val errors = result.eResource.errors
 		Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
-		
+
 		val PythonCompiler cmpPython = new PythonCompiler(result)
 		cmpPython.compileAndRun
 	}
-	
+
 	@Test
 	def void testSVMReg() {
 		val result = parseHelper.parse('''
-			regression{
-				file: "datasets/salary_data.csv",
-				testSize : 3,
-				predictiveVariables : {0},
-				 targetVariable: 1,
-				 algorithm: SVM,
-				 errorType: mean_squared_error
-			}		''')
+		regression{
+			file: "datasets/salary_data.csv",
+			testSize : 3,
+			predictiveVariables : {0},
+			 targetVariable: 1,
+			 algorithm: SVM,
+			 errorType: mean_squared_error
+		}		''')
 		Assertions.assertNotNull(result)
 		val errors = result.eResource.errors
 		Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
-		
+
 		val PythonCompiler cmpPython = new PythonCompiler(result)
 		cmpPython.compileAndRun
 	}
-	
+
 	@Test
 	def void testTreeReg() {
 		val result = parseHelper.parse('''
-			regression{
-				file: "datasets/salary_data.csv",
-				testSize : 3,
-				predictiveVariables : {0},
-				 targetVariable: 1,
-				 algorithm: RegressionTree,
-				 errorType: mean_squared_error
-			}		''')
+		regression{
+			file: "datasets/salary_data.csv",
+			testSize : 3,
+			predictiveVariables : {0},
+			 targetVariable: 1,
+			 algorithm: RegressionTree,
+			 errorType: mean_squared_error
+		}		''')
 		Assertions.assertNotNull(result)
 		val errors = result.eResource.errors
 		Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
-		
+
 		val PythonCompiler cmpPython = new PythonCompiler(result)
 		cmpPython.compileAndRun
 	}
--- org.xtext.idmGH.mlregDsl.tests/src/org/xtext/idmGH/mlregDsl/tests/PythonCompiler.java
+++ org.xtext.idmGH.mlregDsl.tests/src/org/xtext/idmGH/mlregDsl/tests/PythonCompiler.java
@@ -4,24 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.emf.common.util.EList;
 import org.xtext.idmGH.mlregDsl.mLReg.ColVar;
 import org.xtext.idmGH.mlregDsl.mLReg.MlRegression;
 import org.xtext.idmGH.mlregDsl.mLReg.Model;
-import org.xtext.idmGH.mlregDsl.mLReg.PredictiveVars;
 import com.google.common.io.Files;
 
 public class PythonCompiler {
-	
+
 	private Model model;
 
-	PythonCompiler(Model model) {	
-		this.model = model;	
+	PythonCompiler(Model model) {
+		this.model = model;
 	}
-	
+
 	public void compileAndRun() throws IOException {
 
 		MlRegression mlRegression = this.model.getMlRegression();
@@ -33,80 +29,70 @@ public class PythonCompiler {
 			colVarsString += colvar.getIdCol();
 			colVarsString += ",";
 		}
-		colVarsString.replaceAll(",$","");
+		colVarsString.replaceAll(",$", "");
 		int targetVar = mlRegression.getTargetVar().getTargetVar().getIdCol();
 		String algo = mlRegression.getAlgorithm().getAlgoName().getName();
 		String errorMeasure = mlRegression.getErrorMeasure().getErrorMeasure().getName();
-		
-		String pythonCode = "import pandas as pd\n"
-				+ "import matplotlib.pyplot as plt\n"
-				+ "from sklearn.model_selection import train_test_split\n"
-				+ "from sklearn import tree\n"
-				+ "from sklearn.linear_model import LinearRegression\n"
-				+ "from sklearn import svm\n"
-				+ "from sklearn.metrics import r2_score\n"
-				+ "from sklearn.metrics import explained_variance_score\n"
-				+ "from sklearn.metrics import mean_squared_error\n"
-				+ "df = pd.read_csv(\"" + csvFile + "\")\n";//, header=None)\n";
-		
-		
+
+		String pythonCode = "import pandas as pd\n" + "import matplotlib.pyplot as plt\n"
+				+ "from sklearn.model_selection import train_test_split\n" + "from sklearn import tree\n"
+				+ "from sklearn.linear_model import LinearRegression\n" + "from sklearn import svm\n"
+				+ "from sklearn.metrics import r2_score\n" + "from sklearn.metrics import explained_variance_score\n"
+				+ "from sklearn.metrics import mean_squared_error\n" + "df = pd.read_csv(\"" + csvFile + "\")\n";// ,
+																													// header=None)\n";
+
 		// Spliting dataset between features (X) and label (y)
 		pythonCode += "X = df.iloc[: ,[" + colVarsString + "]]\n";
 		pythonCode += "y = df.iloc[: ," + targetVar + "]\n";
-		
 
 		// Spliting into LearningSet and TestSet
-		System.out.println("test size : "+testSize);
-		pythonCode += "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0." + testSize + ", random_state=0)\n";
-		
+		System.out.println("test size : " + testSize);
+		pythonCode += "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0." + testSize
+				+ ", random_state=0)\n";
+
 		// Set algorithm to use
-		 if (algo.equalsIgnoreCase("linear")) {
+		if (algo.equalsIgnoreCase("linear")) {
 			pythonCode += "mlreg = LinearRegression()\n";
-		}else if (algo.equalsIgnoreCase("svm")) {
+		} else if (algo.equalsIgnoreCase("svm")) {
 			pythonCode += "mlreg = svm.SVR()\n";
-		}else if(algo.equalsIgnoreCase("regressiontree")) {
+		} else if (algo.equalsIgnoreCase("regressiontree")) {
 			pythonCode += "mlreg = tree.DecisionTreeRegressor()\n";
 		}
-		
+
 		// Use the algorithm to create a model with the training set
 		pythonCode += "mlreg.fit(X_train, y_train)\n";
-		
+
 		// Prediction
 		pythonCode += "y_prediction = mlreg.predict(X_test)\n";
 		pythonCode += "df_prediction = pd.DataFrame({'Actual': y_test, 'Predicted': y_prediction})\n";
 		pythonCode += "print(\"df_prediction : \", df_prediction)\n";
-				
-		
-		
+
 		// Compute and display the error
 		// Set error measure to use
-		 if (errorMeasure.equalsIgnoreCase("mean_squared_error")) {
+		if (errorMeasure.equalsIgnoreCase("mean_squared_error")) {
 			pythonCode += "error = mean_squared_error(y_test, y_prediction)\n";
-		}else if (errorMeasure.equalsIgnoreCase("explained_variance_score")) {
+		} else if (errorMeasure.equalsIgnoreCase("explained_variance_score")) {
 			pythonCode += "error = explained_variance_score(y_test, y_prediction)\n";
-		}else if(errorMeasure.equalsIgnoreCase("r2_score")) {
+		} else if (errorMeasure.equalsIgnoreCase("r2_score")) {
 			pythonCode += "error = r2_score(y_test, y_prediction)\n";
 		}
 		pythonCode += "print(\"Error : \", error)\n";
-		
-		
+
 		// serialize code into Python filename
 
-		csvFile = csvFile.substring(csvFile.lastIndexOf("/")).replace("/","");
-		String PYTHON_OUTPUT = "python_outputs/" + csvFile.replaceAll(".csv", "") + "_" + algo + ".py";			
+		csvFile = csvFile.substring(csvFile.lastIndexOf("/")).replace("/", "");
+		String PYTHON_OUTPUT = "python_outputs/" + csvFile.replaceAll(".csv", "") + "_" + algo + ".py";
 		Files.write(pythonCode.getBytes(), new File(PYTHON_OUTPUT));
 		Process p = Runtime.getRuntime().exec("python " + PYTHON_OUTPUT);
-	    BufferedReader stdInput = new BufferedReader(new 
-	         InputStreamReader(p.getInputStream()));
-	    BufferedReader stdError = new BufferedReader(new 
-	         InputStreamReader(p.getErrorStream()));
-	    String o;
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		String o;
 		while ((o = stdInput.readLine()) != null) {
-	        System.out.println(o);
-	    }	    
-		String err; 
+			System.out.println(o);
+		}
+		String err;
 		while ((err = stdError.readLine()) != null) {
-	        System.out.println(err);
-	    }
+			System.out.println(err);
+		}
 	}
 }
